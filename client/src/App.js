53a810afa7e356af4afc658933afde3e15c63a8b
Aung Khant Kyaw <93343553+AungKyaw321@users.noreply.github.com>
2024-03-01 09:56:59 -0800

    Commit

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import React, { Fragment } from "react";
+//components
+import ListTodos from "./components/ListTodos";
+import InputTodo from "./components/InputTodo";
+
+function App() {
+  return (
+    <Fragment>
+      <div className="container">
+        <InputTodo />
+        <ListTodos />
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
